5108a55b6f5af2c92bbcc108328e7ad5865bca2b
mbakhmetiev <mbakhmetiev@yandex.ru>
2023-08-18 19:43:30 +0200

    xls done train

--- genreport.py
+++ genreport.py
@@ -5,6 +5,7 @@ import pandas as pd
 from collections import OrderedDict
 import requests
 import yaml
+from datetime import datetime
 
 pd.set_option('display.max_colwidth', None)
 
@@ -21,14 +22,25 @@ def response(payload):
   return pd.json_normalize([item['data'] for item in response] )
 
 def result(accgr, **params):
-  
+
+  columns = ['Month', 'Opco', 'Cloud type', 'Subscription/account name', \
+             'Subscription ID/account ID', 'ARN', 'Resource type', 'Policy' \
+             'Standard', 'Severity', 'Passed' ]
+    
+  df_xls = pd.DataFrame(columns=columns)
+
   def df_to_xls(df, passed ):
-    res = df['accountName', 'accountId', 'rrn', 'resourceType']
-    res.rename(columns = {'accountName':'Subscription/account name', \
-                          'accountId':'Subscription ID/account ID', \
-                          'rrn':'ARN',\
-                          'resourceType':'Resource type'} )
-    res.insert(0, ) 
+    df_to_xls = pd.DataFrame(columns=columns)
+    df_to_xls[['Cloud type', 'Subscription/account name', 'Subscription ID/account ID', 'ARN', 'Resource type']] = \
+    df[['cloudType', 'accountName', 'accountId', 'rrn', 'resourceType' ]]
+    df_to_xls['Month'] = f"{datetime.now().month}/{datetime.now().year}"
+    df_to_xls['Opco'] = "Opco"
+    df_to_xls['Policy'] = params['policy']
+    df_to_xls['Standard'] = params['info']
+    df_to_xls['Severity'] = params['severity']
+    df_to_xls['Passed'] = passed
+
+    return df_to_xls
 
   rql1 = params['rql1'] % accgr
   rql2 = params['rql2'] % accgr
@@ -39,25 +51,28 @@ def result(accgr, **params):
   txt1 = f"Total number of assets: {len(df2)}"
 
   if df1.empty and not df2.empty:
-    df_to_xls(df2, True)
-    txt2 = f"Pass: {len(df2)}"
-    txt3 = f"Fail: {len(df1)}"
-    assetspass = f"\nPassed assets:\n{df2[params['display_on']].to_string(header=False, index=False)}"
-    assetsfail = f"\nFailed assets: None\n"
-    return f"{txt1}\n{txt2}\n{txt3}\n{assetspass}\n{assetsfail}\n"
+    df_xls.append(df_to_xls(df2, "Passed"))
+    #txt2 = f"Pass: {len(df2)}"
+    #txt3 = f"Fail: {len(df1)}"
+    #assetspass = f"\nPassed assets:\n{df2[params['display_on']].to_string(header=False, index=False)}"
+    #assetsfail = f"\nFailed assets: None\n"
+    #return f"{txt1}\n{txt2}\n{txt3}\n{assetspass}\n{assetsfail}\n"
 
   if not df1.empty and not df2.empty:
     total = df2[params['display_on']]
     failed = df1[params['display_on']]
+    df_xls.append(df_to_xls(failed, "Failed"))
     passed = pd.concat([total,failed]).drop_duplicates(keep=False)
-    txt2 = f"Pass: {len(passed)}"
-    txt3 = f"Fail: {len(failed)}"
-    assetspass = f"\nPassed assets:\n{passed.to_string(header=False, index=False)}"
-    assetsfail = f"\nFailed assets:\n{failed.to_string(header=False, index=False)}"
-    return f"{txt1}\n{txt2}\n{txt3}\n{assetspass}\n{assetsfail}\n"
+    df_xls.append(df_to_xls(passed, "Passed"))
+    #txt2 = f"Pass: {len(passed)}"
+    #txt3 = f"Fail: {len(failed)}"
+    #assetspass = f"\nPassed assets:\n{passed.to_string(header=False, index=False)}"
+    #assetsfail = f"\nFailed assets:\n{failed.to_string(header=False, index=False)}"
+    #return f"{txt1}\n{txt2}\n{txt3}\n{assetspass}\n{assetsfail}\n"
   
-  if df1.empty and df2.empty:
-    return f"{txt1}\n"
+  #if df1.empty and df2.empty:
+  #  return f"{txt1}\n"
+  df_xls.to_excel(f"{accgr}.xlsx", index=False)
 
 with open('accgroups.txt') as f:
     accgroups = f.readlines()
@@ -70,14 +85,15 @@ for k, accgr in enumerate(accgroups):
     outdict.update({k:{ 'name': accgr.strip(), 'output':[] }})
     for std in standards:
         for section in standards[std]:
-            outdict[k]['output'].append(standards[std][section]['info'])
-            outdict[k]['output'].append(result(accgr.strip(), **standards[std][section]))
-            outdict[k]['output'].append('-'*145) # line of 145 *, cosmetic
+            #outdict[k]['output'].append(standards[std][section]['info'])
+            result(accgr.strip(), **standards[std][section])
+            #outdict[k]['output'].append(result(accgr.strip(), **standards[std][section]))
+            #outdict[k]['output'].append('-'*145) # line of 145 *, cosmetic
      
-env = Environment(loader=FileSystemLoader('templates'))
-template = env.get_template('report.j2')
+#env = Environment(loader=FileSystemLoader('templates'))
+#template = env.get_template('report.j2')
 
-for k in outdict:
-    report = f"{outdict[k]['name']}.txt"
-    with open(report, 'a') as f:
-        f.write(template.render(outdict[k],trim_blocks=True, lstrip_blocks=True))
+#for k in outdict:
+#    report = f"{outdict[k]['name']}.txt"
+#    with open(report, 'a') as f:
+#        f.write(template.render(outdict[k],trim_blocks=True, lstrip_blocks=True))
